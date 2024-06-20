d4be134937af3d21dd4bb998f930acf4f526a0c8
imsamrth <hiimsamrth@users.noreply.github.com>
2024-06-21 00:52:48 +0530

    Remove env

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -37,10 +37,7 @@ func DBinstance() *mongo.Client {
 }
 
 func DBAtlas() *mongo.Client {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+
 	MongoDB := os.Getenv("MONGODB_URL")
 	log.Println(MongoDB)
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
